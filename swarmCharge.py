c38ca3ee52501ceca2949b9686298faf9269e589
Zdenek Boucek <zboucek@ntis.zcu.cz>
2022-05-31 18:28:43 +0200

    modify swarmSequence example Logger and SyncCrazyflie werent working together

---
+++ swarmCharge.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2017-2018 Bitcraze AB
+#
+#  Crazyflie Nano Quadcopter Client
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+"""
+Version of the AutonomousSequence.py example connecting to 10 Crazyflies.
+The Crazyflies go straight up, hover a while and land but the code is fairly
+generic and each Crazyflie has its own sequence of setpoints that it files
+to.
+
+"""
+import time
+
+import cflib.crtp
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.swarm import CachedCfFactory
+from cflib.crazyflie.swarm import Swarm
+from cflib.crazyflie.syncLogger import SyncLogger
+
+# Parameters
+HEIGHT = 0.5 # z-global [meters]
+VEL_Z = -0.5 # landing velocity [m/s]
+
+# Change uris and sequences according to your setup
+URI1 = 'radio://0/100/2M/E7E7E7E701'
+URI2 = 'radio://0/100/2M/E7E7E7E702'
+URI3 = 'radio://0/100/2M/E7E7E7E703'
+URI4 = 'radio://0/100/2M/E7E7E7E704'
+
+# List of URIs, comment the one you do not want to fly
+uris = {
+    URI1,
+    URI2,
+    URI3,
+    URI4
+}
+
+CF_IN_AIR = False # safety variable (only one drone at a time)
+
+def wait_for_position_estimator(scf):
+    print('Waiting for estimator to find position...')
+
+    log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+    log_config.add_variable('kalman.varPX', 'float')
+    log_config.add_variable('kalman.varPY', 'float')
+    log_config.add_variable('kalman.varPZ', 'float')
+
+    var_y_history = [1000] * 10
+    var_x_history = [1000] * 10
+    var_z_history = [1000] * 10
+
+    threshold = 0.001
+
+    with SyncLogger(scf, log_config) as logger:
+        for log_entry in logger:
+            data = log_entry[1]
+
+            var_x_history.append(data['kalman.varPX'])
+            var_x_history.pop(0)
+            var_y_history.append(data['kalman.varPY'])
+            var_y_history.pop(0)
+            var_z_history.append(data['kalman.varPZ'])
+            var_z_history.pop(0)
+
+            min_x = min(var_x_history)
+            max_x = max(var_x_history)
+            min_y = min(var_y_history)
+            max_y = max(var_y_history)
+            min_z = min(var_z_history)
+            max_z = max(var_z_history)
+
+            # print("{} {} {}".
+            #       format(max_x - min_x, max_y - min_y, max_z - min_z))
+
+            if (max_x - min_x) < threshold and (
+                    max_y - min_y) < threshold and (
+                    max_z - min_z) < threshold:
+                break
+
+def reset_estimator(scf):
+    cf = scf.cf
+    cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    cf.param.set_value('kalman.resetEstimation', '0')
+
+    wait_for_position_estimator(cf)
+    
+def set_initial_position(self, scf, x, y, z, yaw_radians = 0.0):
+    scf.cf.param.set_value('kalman.initialX', x)
+    scf.cf.param.set_value('kalman.initialY', y)
+    scf.cf.param.set_value('kalman.initialZ', z)
+    scf.cf.param.set_value('kalman.initialYaw', yaw_radians)
+
+
+def take_off(cf, position):
+    take_off_time = 1.0
+    sleep_time = 0.1
+    steps = int(take_off_time / sleep_time)
+    vz = position[2] / take_off_time
+
+    print(vz)
+
+    for i in range(steps):
+        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+        time.sleep(sleep_time)
+    
+    cf.commander.send_position_setpoint(0, 0, HEIGHT, 0)
+
+
+def land(cf, position):
+    landing_time = 1.0
+    sleep_time = 0.1
+    steps = int(landing_time / sleep_time)
+    vz = -position[2] / landing_time
+
+    print(vz)
+
+    for _ in range(steps):
+        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+        time.sleep(sleep_time)
+
+    cf.commander.send_stop_setpoint()
+    # Make sure that the last packet leaves before the link is closed
+    # since the message queue is not flushed before closing
+    time.sleep(0.1)
+
+
+def run_sequence(scf, sequence):
+    if not CF_IN_AIR:
+        CF_IN_AIR = True
+        try:
+            cf = scf.cf
+
+            take_off(cf, sequence[0])
+            # for position in sequence:
+            #     print('Setting position {}'.format(position))
+            #     end_time = time.time() + position[3]
+            #     while time.time() < end_time:
+            #         cf.commander.send_position_setpoint(position[0],
+            #                                             position[1],
+            #                                             position[2], 0)
+            #         time.sleep(0.1)
+            land(cf, HEIGHT)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG)
+    cflib.crtp.init_drivers()
+
+    factory = CachedCfFactory(rw_cache='./cache')
+    with Swarm(uris, factory=factory) as swarm:
+        swarm.parallel(reset_estimator)
+        while True:
+            swarm.parallel(run_sequence)
